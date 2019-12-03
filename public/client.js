486d1012e1d4f034dc198243b46409754f94addf
Glitch (longing-eel) <none>
2019-12-02 22:02:05 -0800

    finally

--- public/client.js
+++ public/client.js
@@ -1,73 +1,70 @@
-let people = [];
-const submitted = localStorage.getItem("submitted");
+let people = []
 
 // define variables that reference elements on our page
-const peopleForm = document.forms[0];
-const nameInput = peopleForm.elements["name"];
-const phoneInput = peopleForm.elements["phone"];
-const peopleList = document.getElementById("people");
-const peopleView = document.getElementById("people-section")
-const afterSubmit = document.getElementById("after-submit");
-const clearButton = document.querySelector("#clear-people");
-
-// const members = ['Dean Dudzik','Jackson Fortin','Xya Ford','Logan Richards','Bree Ewert','Emma Hightower','Annika Brown','Kaci Rose','Zoe Sauvageau', 'Zane Davis-Barrs']
-// const nameOfTheDay = members[Math.floor(Math.random()*members.length)]
-// nameInput.placeholder = nameOfTheDay
+const peopleForm = document.forms[0]
+const nameInput = peopleForm.elements['name']
+const phoneInput = peopleForm.elements['phone']
+const peopleList = document.getElementById('people')
+const peopleView = document.getElementById('people-section')
+const afterSubmit = document.getElementById('after-submit')
+const clearButton = document.querySelector('#clear-people')
 
 // request the people from our app's sqlite database
 const getPeople = () => {
-  fetch("/getPeople", {})
+  fetch('/getPeople', {})
     .then(res => res.json())
     .then(response => {
-      people = response.people;
+      peopleList.innerHTML = ''
+
+      people = response.people
       if (people.length > 0) {
-        peopleView.style.display = "block";
+        peopleView.style.display = 'block'
         people.forEach(row => {
-          appendNewPerson(row.name);
-        });
+          appendNewPerson(row.name)
+        })
       } else {
-        peopleView.style.display = "none";
+        peopleView.style.display = 'none'
       }
-    
+
       // don't allow users to resubmit
-      if (submitted == "true" && people.length > 0) {
-        peopleForm.style.display = "none";
-        afterSubmit.style.display = "block";
+      if (localStorage.getItem('submitted') == 'true' && people.length > 0) {
+        peopleForm.style.display = 'none'
+        afterSubmit.style.display = 'block'
       } else {
-        peopleForm.style.display = "block";
-        afterSubmit.style.display = "none";
+        peopleForm.style.display = 'block'
+        afterSubmit.style.display = 'none'
       }
-    });
+    })
 }
-  
+
 // a helper function that creates a list item for a given person
 const appendNewPerson = name => {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = name;
-  peopleList.appendChild(newListItem);
-};
+  const newListItem = document.createElement('li')
+  newListItem.innerText = name
+  peopleList.appendChild(newListItem)
+}
 
 getPeople()
 
 // listen for the form to be submitted and add a new person when it is
 peopleForm.onsubmit = event => {
   // stop our form submission from refreshing the page
-  event.preventDefault();
+  event.preventDefault()
 
-  const data = { name: nameInput.value, phone: phoneInput.value };
+  const data = { name: nameInput.value, phone: phoneInput.value }
 
-  fetch("/addPerson", {
-    method: "POST",
+  fetch('/addPerson', {
+    method: 'POST',
     body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" }
+    headers: { 'Content-Type': 'application/json' }
   })
     .then(res => res.json())
     .then(response => {
       // reset form
-      nameInput.value = "";
-      phoneInput.value = "";
+      nameInput.value = ''
+      phoneInput.value = ''
 
-      localStorage.setItem("submitted", "true");
+      localStorage.setItem('submitted', 'true')
       getPeople()
-    });
-};
+    })
+}
--- server.js
+++ server.js
@@ -1,189 +1,189 @@
-require("dotenv").config();
+require('dotenv').config()
 
 const {
   APP_AUTH_TOKEN,
   TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN,
   TWILIO_MESSAGING_SERVICE_SID
-} = process.env;
+} = process.env
 
-const Twilio = require("twilio");
-const validator = require("validator");
-const twilio = Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+const Twilio = require('twilio')
+const validator = require('validator')
+const twilio = Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const fs = require("fs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const fs = require('fs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 // init sqlite db
-const dbFile = "./.data/sqlite2.db";
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(dbFile);
+const dbFile = './.data/sqlite.db'
+const exists = fs.existsSync(dbFile)
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database(dbFile)
 
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(() => {
   if (!exists) {
     db.run(
-      "CREATE TABLE People (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT, assigned_name TEXT)"
-    );
-    console.log("New table People created!");
+      'CREATE TABLE People (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT, assigned_name TEXT)'
+    )
+    console.log('New table People created!')
 
     // insert default person
     db.serialize(() => {
       db.run(
         'INSERT INTO People (name,phone) VALUES ("Zane","***REMOVED***","")'
-      );
-    });
+      )
+    })
   } else {
-    console.log('Database "People" ready to go!');
-    db.each("SELECT * from People", (err, row) => {
+    console.log('Database "People" ready to go!')
+    db.each('SELECT * from People', (err, row) => {
       if (row) {
-        console.log(`record: ${row.name}`);
+        console.log(`record: ${row.name}`)
       }
-    });
+    })
   }
-});
+})
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(`${__dirname}/views/index.html`);
-});
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/views/index.html`)
+})
 
-app.get("/admin", (request, response) => {
-  response.sendFile(`${__dirname}/views/admin.html`);
-});
+app.get('/admin', (request, response) => {
+  response.sendFile(`${__dirname}/views/admin.html`)
+})
 
 // endpoint to get all the people in the database
-app.get("/getPeople", (req, res) => {
-  db.all("SELECT * from People", (err, rows) => {
-    if (req.get("Authorization")) {
+app.get('/getPeople', (req, res) => {
+  db.all('SELECT * from People', (err, rows) => {
+    if (req.get('Authorization')) {
       // if token is supplied, send full data, otherwise filter
-      if (authenticate(req.get("Authorization").split(" ")[1])) {
-        res.send(JSON.stringify({ status: 200, people: rows }));
+      if (authenticate(req.get('Authorization').split(' ')[1])) {
+        res.send(JSON.stringify({ status: 200, people: rows }))
       } else {
-        res.status(401).json({ status: 401, error: "invalid auth token" });
+        res.status(401).json({ status: 401, error: 'invalid auth token' })
       }
     } else {
-      const filteredPeople = [];
+      const filteredPeople = []
       rows.forEach(row => {
-        filteredPeople.push({ name: row.name });
-      });
-      res.send(JSON.stringify({ status: 200, people: filteredPeople }));
+        filteredPeople.push({ name: row.name })
+      })
+      res.send(JSON.stringify({ status: 200, people: filteredPeople }))
     }
-  });
-});
+  })
+})
 
 // endpoint to add a person to the database
-app.post("/addPerson", (request, response) => {
-  console.log(`add to people ${request.body}`);
+app.post('/addPerson', (request, response) => {
+  console.log(`add to people ${request.body}`)
 
   // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
   if (!process.env.DISALLOW_WRITE) {
-    const cleansedName = cleanseString(request.body.name);
-    const cleansedPhone = cleanseString(request.body.phone);
+    const cleansedName = cleanseString(request.body.name)
+    const cleansedPhone = cleanseString(request.body.phone)
     db.run(
       `INSERT INTO People (name,phone,assigned_name) VALUES (?,?,?)`,
       cleansedName,
       cleansedPhone,
-      "",
+      '',
       error => {
         if (error) {
-          response.send({ message: "error!" });
+          response.send({ message: 'error!' })
         } else {
-          response.send({ message: "success" });
+          response.send({ message: 'success' })
         }
       }
-    );
+    )
   }
-});
+})
 
-app.use("/*", (req, res, next) => {
+app.use('/*', (req, res, next) => {
   // only allow authenticated users to access API
-  if (req.get("Authorization")) {
+  if (req.get('Authorization')) {
     // for Bearer token authorization, extract the token
-    if (authenticate(req.get("Authorization").split(" ")[1])) {
-      next();
+    if (authenticate(req.get('Authorization').split(' ')[1])) {
+      next()
     } else {
-      res.status(401).json({ status: 401, error: "permission denied" });
+      res.status(401).json({ status: 401, error: 'permission denied' })
     }
   } else {
-    res.status(401).json({ status: 401, error: "authorization required" });
+    res.status(401).json({ status: 401, error: 'authorization required' })
   }
-});
+})
 
 // endpoint to clear people from the database
-app.post("/clearPeople", (request, response) => {
+app.post('/clearPeople', (request, response) => {
   // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
   if (!process.env.DISALLOW_WRITE) {
     db.each(
-      "SELECT * from People",
+      'SELECT * from People',
       (err, row) => {
-        console.log("row", row);
+        console.log('row', row)
         db.run(`DELETE FROM People WHERE ID=?`, row.id, error => {
           if (row) {
-            console.log(`deleted row ${row.id}`);
+            console.log(`deleted row ${row.id}`)
           }
-        });
+        })
       },
       err => {
         if (err) {
-          response.send({ message: "error!" });
+          response.send({ message: 'error!' })
         } else {
-          response.send({ message: "success" });
+          response.send({ message: 'success' })
         }
       }
-    );
+    )
   }
-});
+})
 
-app.post("/deletePerson", (req, res) => {
+app.post('/deletePerson', (req, res) => {
   if (req.body.id) {
     db.run(`DELETE FROM People WHERE ID=?`, req.body.id, error => {
       if (error) {
-        res.status(400).send({ status: 400, message: "error! does id exist?" });
+        res.status(400).send({ status: 400, message: 'error! does id exist?' })
       } else {
-        res.status(200).send({ status: 200, message: "success" });
+        res.status(200).send({ status: 200, message: 'success' })
       }
-    });
+    })
   } else {
-    res.status(400).send({ status: 400, message: "requires a user id" });
+    res.status(400).send({ status: 400, message: 'requires a user id' })
   }
-});
+})
 
-app.post("/assignSecretSanta", (req, res) => {
-  db.all("SELECT * from People", (err, rows) => {
+app.post('/assignSecretSanta', (req, res) => {
+  db.all('SELECT * from People', (err, rows) => {
     const filteredRows = rows.filter(
       row =>
-        !row.name.toLowerCase().includes("xya") &&
-        !row.name.toLowerCase().includes("kate") &&
-        !row.name.toLowerCase().includes("zoe") &&
-        !row.name.toLowerCase().includes("kaci")
-    );
-    const names = filteredRows.map(row => row.name);
-    const picks = getPicks(names);
+        !row.name.toLowerCase().includes('xya') &&
+        !row.name.toLowerCase().includes('kate') &&
+        !row.name.toLowerCase().includes('zoe') &&
+        !row.name.toLowerCase().includes('kaci')
+    )
+    const names = filteredRows.map(row => row.name)
+    const picks = getPicks(names)
 
     rows.forEach(row => {
-      [
-        ["xya", "kate"],
-        ["kate", "xya"],
-        ["zoe", "kaci"],
-        ["kaci", "zoe"]
+      ;[
+        ['xya', 'kate'],
+        ['kate', 'xya'],
+        ['zoe', 'kaci'],
+        ['kaci', 'zoe']
       ].forEach(per => {
         if (row.name.toLowerCase().includes(per[0])) {
           const assigned_name = rows.filter(row =>
             row.name.toLowerCase().includes(per[1])
-          )[0].name;
-          picks.push({ name: row.name, assigned_name });
+          )[0].name
+          picks.push({ name: row.name, assigned_name })
         }
-      });
-    });
+      })
+    })
 
     picks.forEach(pick => {
       db.run(
@@ -192,17 +192,17 @@ app.post("/assignSecretSanta", (req, res) => {
         pick.name,
         (err, rows) => {
           if (rows) {
-            console.log(rows);
+            console.log(rows)
           }
         }
-      );
-    });
-    res.status(200).send({ status: 200 });
-  });
-});
+      )
+    })
+    res.status(200).send({ status: 200 })
+  })
+})
 
-app.post("/sendText", (req, res) => {
-  db.all("SELECT * from People", (err, rows) => {
+app.post('/sendText', (req, res) => {
+  db.all('SELECT * from People', (err, rows) => {
     Promise.all(
       rows.map(row => {
         if (validator.isMobilePhone(row.phone)) {
@@ -210,45 +210,45 @@ app.post("/sendText", (req, res) => {
             to: row.phone,
             from: TWILIO_MESSAGING_SERVICE_SID,
             body: `Good morrow, sirrah! Thou hath been assigned ${row.assigned_name.toUpperCase()} for thine Secret Santa 🎅🎁`
-          });
+          })
         }
-        return;
+        return
       })
     )
       .then(() => {
-        res.status(200).send({ status: 200, message: "messages sent!" });
+        res.status(200).send({ status: 200, message: 'messages sent!' })
       })
       .catch(err => {
-        res.status(500).send({ status: 500, message: err.toString() });
-      });
-  });
-});
+        res.status(500).send({ status: 500, message: err.toString() })
+      })
+  })
+})
 
-// https://stackoverflow.com/a/21295633
+// modified from https://stackoverflow.com/a/21295633
 const getPicks = names => {
   return names
     .slice(0)
     .sort(function() {
-      return Math.random() - 0.5;
+      return Math.random() - 0.5
     })
     .map(function(name, index, arr) {
-      const assigned_name = arr[(index + 1) % arr.length];
-      return { name, assigned_name };
-    });
-};
+      const assigned_name = arr[(index + 1) % arr.length]
+      return { name, assigned_name }
+    })
+}
 
 // helper function that prevents html/css/script malice
 const cleanseString = function(string) {
-  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-};
+  return string.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+}
 
 // helper function to authenticate with token
 const authenticate = token => {
-  const tokens = APP_AUTH_TOKEN.split(",");
-  return tokens.includes(token);
-};
+  const tokens = APP_AUTH_TOKEN.split(',')
+  return tokens.includes(token)
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
-});
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
